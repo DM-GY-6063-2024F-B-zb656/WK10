a424bc905f1c2acb7b09ea9790e549e3e9c125ff
zb656 <145730190+zb656@users.noreply.github.com>
2024-11-11 19:38:15 -0500

    classes

--- 0/sketch.js
+++ 0/sketch.js
@@ -3,6 +3,39 @@
 
 let blinks = [];
 
+class Blinky {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.visible = false;
+    this.period = random(100, 1000);
+    this.changed = 0;
+    this.diam = 50;
+    this.mocolor = color(255,0,0);
+    this.mouseOver = false;
+  }
+
+  update() {
+    if (millis() - this.changed > this.period) {
+      this.visible = !this.visible;
+      this.changed = millis();
+    }
+
+    this.mouseOver = dist(mouseX, mouseY, this.x, this.y) < (this.diam/2);
+  }
+
+  draw() {
+    if (this.visible || this.mouseOver) {
+      if(this.mouseOver) {
+        fill(this.mocolor);
+      } else {
+        fill(255);
+      }
+      ellipse(this.x, this.y, this.diam);
+    }
+  }
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
@@ -12,24 +45,11 @@ function draw() {
   background(200, 20, 120);
   for (let idx = 0; idx < blinks.length; idx++){
     let mBlink = blinks[idx];
-    if (millis() - mBlink.changed > mBlink.period) {
-      mBlink.visible = !mBlink.visible;
-      mBlink.changed = millis();
-    }
-
-    if (mBlink.visible) {
-      ellipse(mBlink.x, mBlink.y, mBlink.diam);
-    }
+    mBlink.update();
+    mBlink.draw();
   }
 }
 
 function mousePressed() {
-  blinks.push({
-    x: mouseX,
-    y: mouseY,
-    visible: false,
-    period: random(100, 1000),
-    changed: 0,
-    diam: 50,
-  });
+  blinks.push(new Blinky(mouseX, mouseY));
 }
